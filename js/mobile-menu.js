a735e10a82c0356680e4d5ec3755ced96a4a56fb
LucaMuntele16 <lucamuntele@yahoo.com>
2024-02-16 20:04:30 +0200

    responsive done

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -12,3 +12,17 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-openx]"),
+    closeModalBtn: document.querySelector("[data-modal-closex]"),
+    modal: document.querySelector("[data-modalx]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
